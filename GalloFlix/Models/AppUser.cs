0077a30de02fb7004bf042474e35a84f715ebb56
prestes21 <bianca2102prestes@gmail.com>
2023-04-20 11:41:10 -0300

    Adicionado os pacotes de conexão com banco de dados e identidade ; Criando a classe de usuário

---
+++ GalloFlix/Models/AppUser.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace GalloFlix.Models;
+public class AppUser : IdentityUser 
+{
+    [Required]
+    [StringLength(60)]
+    public string Name { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+
+    public string UserName { get; set;}
+    public string ProfilePicture { get; set; }
+
+}
