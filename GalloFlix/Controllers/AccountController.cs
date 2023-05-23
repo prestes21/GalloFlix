c2545ac8ba96dcc5ffe5f1ac0c2f33269f840bff
prestes21 <bianca2102prestes@gmail.com>
2023-05-23 11:35:13 -0300

    codificação do login

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using GalloFlix.DataTransferObjects;
 using GalloFlix.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -35,8 +36,50 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public IActionResult Login()
+    [AllowAnonymous]
+    public async Task<IActionResult> Login(LoginDto login)
     {
-        return View();
+        // Verificar o modelo a fazer o login
+        if(ModelState.IsValid) //Validação do lado do servidor
+        {
+            string userName = login.Email;
+            if(IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+                                                                                                            //Operadores Lógicos
+                                                                                                            //$$ - e  ;|| - ou ;! - não
+            }
+            var result = await _singInManager.PasswordSignInAsync(
+                userName, login.Password, login.RemenberMe, lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário { login.Email} acessou o sistema");
+                return LocalRedirect(login.ReturnUrl);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário { login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
+            }
+            ModelState.AddModelError("login", "Usuário e/ou Senha Inválidos!!!");
+        }
+        return View(login);
+    }
+
+    private bool IsValidEmail(string email)
+    { 
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+
     }
 }
